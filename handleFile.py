0315967a77c6e1f1f8a42dbf0572ba12e0cb2de9
TECPan <pw_seu@foxmail.com>
2016-11-04 16:29:01 +0800

    test

---
+++ handleFile.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Passion'
+f = open(r'C:\Users\Passion\Desktop\5.txt')
+w = open(r'C:\Users\Passion\Desktop\55.txt', 'w')
+num=1
+line = f.readline()
+while (len(line) != 0):
+    print(line)
+    if(num%2 ==0):
+        w.write(line)
+    num=num+1
+    line = f.readline()
+f.close()
+w.close()
